a062e08118500352249af3a0657c656be59cb0a5
DamonLC21 <chiversdamon@gmail.com>
2020-07-23 12:50:55 -0600

    hooks useState and useEffect implemented for timer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(0)
+  
+  const increment = () => setCount(count + 1)
+  const decrement = () => setCount(count - 1)
+
+  useEffect(() => {
+    let interval = setInterval(increment, 1000)
+
+    return () => clearInterval(interval)
+  }, [count])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter App</h1>
+      <h2>{count}</h2>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
     </div>
   );
 }
